6d705c63a2bb80840d36e16426d6b093efd9bb82
JohanTran <johan.tran0210@gmail.com>
2024-01-16 16:50:24 +0100

    time functions done

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 "use strict";
+
+const seconds = 60;
+
+updateHour();
+updateDate();
+
+setInterval(updateHour, seconds * 1000);
+setInterval(updateDate, 60000);
+
+//Ändra tiden(timmar) med minutprecision
+function updateHour(){
+    updateTimeElement("time");
+}
+
+//Ändra dagen
+function updateDate(){
+    updateTimeElement("date");
+}
+
+function updateTimeElement(element = ""){
+    const timeValue = getTime(element);
+    
+    const timeElement = document.querySelector(`.${element}`);
+    timeElement.innerHTML = timeValue;
+}
+
+//Hämta tiden 
+function getTime(type = ""){
+    const d = new Date();
+    let options = {};
+    
+    if(!type){
+        console.log("invalid input");
+        return;
+    }
+
+    if(type === "time"){
+        options = {
+            hour: "numeric",
+            minute: "numeric"
+        };
+
+        const date_time = d.toLocaleTimeString("sv-SE", options);
+        return date_time;
+    }
+    else if(type === "date"){
+        options = {
+            day: "numeric",
+            month: "long",
+            year: "numeric"
+        };
+
+        const date_date = d.toLocaleDateString("sv-SE", options);
+        return date_date;
+    }
+}
