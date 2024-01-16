db16fbea53884b03994c77908eb5458d4bd6bd0e
JohanTran <johan.tran0210@gmail.com>
2024-01-16 17:31:25 +0100

    added localstorage file

---
+++ storage.js
@@ -0,0 +1,14 @@
+const dashboard_title = document.querySelector(".dashboard-title");
+const dashboard_note_area = document.querySelector(".dashboard-note-area");
+
+// dashboard_title.addEventListener("input", function () {
+//     localStorage.setItem("dashboard-title", dashboard_title.textContent);
+// });
+
+
+// dashboard_note_area.addEventListener("input", function(){
+//     localStorage.setItem("dashboard-note-area", dashboard_note_area.textContent);
+// })
+
+// let title = localStorage.getItem("dashboard-title");
+// dashboard_title.textContent = title;
